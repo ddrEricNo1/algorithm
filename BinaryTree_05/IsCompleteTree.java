5e19bb3247665d9c77acccb420ef13fff37adf90
“ddrEricNo1” <ericDing1811@outlook.com>
2023-03-30 21:48:59 +0800

    2023/3/30

--- BinaryTree_05/IsCompleteTree.java
+++ BinaryTree_05/IsCompleteTree.java
@@ -9,42 +9,75 @@ public class IsCompleteTree {
         public int val;
         public Node left;
         public Node right;
+
         public Node(int val) {
             this.val = val;
         }
-
-        public static boolean isCompleteTree(Node head) {
-            if (head == null) {
-                return true;
-            }
-            Queue<Node> queue = new LinkedList<>();
-            // 是否遇到过左右两个子孩子不全的节点
-            boolean leaf = false;
-            Node l = null;
-            Node r = null;
-            queue.add(head);
-            while (!queue.isEmpty()) {
-                Node cur = queue.poll();
-                l = cur.left;
-                r = cur.right;
-
-                // 如果已经有一个左右子节点不双全的节点，并且后续又遇到了非叶子节点
-                // 或者当前节点有右子节点但没有左子节点
-                if ((leaf && (l != null || r != null)) || (l == null && r != null)) {
-                    return false;
-                }
-
-                if (cur.left != null) {
-                    queue.add(cur.left);
-                }
-                if (cur.right !=null) {
-                    queue.add(cur.right);
-                }
-                if (l == null && r == null) {
-                    leaf = true;
-                }
-            }
+    }
+    public static boolean isCompleteTree(Node head) {
+        if (head == null) {
             return true;
         }
+        Queue<Node> queue = new LinkedList<>();
+        // 是否遇到过左右两个子孩子不全的节点
+        boolean leaf = false;
+        Node l = null;
+        Node r = null;
+        queue.add(head);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            l = cur.left;
+            r = cur.right;
+
+            // 如果已经有一个左右子节点不双全的节点，并且后续又遇到了非叶子节点
+            // 或者当前节点有右子节点但没有左子节点
+            if ((leaf && (l != null || r != null)) || (l == null && r != null)) {
+                return false;
+            }
+
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right !=null) {
+                queue.add(cur.right);
+            }
+            if (l == null && r == null) {
+                leaf = true;
+            }
+        }
+        return true;
+    }
+
+    public static class ReturnData {
+        // 高度
+        public int height;
+
+        // 节点个数
+        public int nodes;
+
+        public ReturnData(int height, int nodes) {
+            this.nodes = nodes;
+            this.height = height;
+        }
+    }
+
+    public static ReturnData process(Node head) {
+        if (head == null) {
+            return new ReturnData(0, 0);
+        }
+        ReturnData leftData = process(head.left);
+        ReturnData rightData = process(head.right);
+        int height = Math.max(leftData.height, rightData.height) + 1;
+        int nodes = leftData.nodes + rightData.nodes + 1;
+        return new ReturnData(height, nodes);
+    }
+
+    // 满二叉树的递归套路
+    public static boolean isFull(Node head) {
+        if (head == null) {
+            return true;
+        }
+        ReturnData info = process(head);
+        return info.nodes == ((1 << info.height) - 1);
     }
 }
--- BinaryTree_05/TestBST.java
+++ BinaryTree_05/TestBST.java
@@ -58,4 +58,44 @@ public class TestBST {
         }
         return true;
     }
+
+    public static class ReturnData {
+        public boolean isBST;
+        public int min;
+        public int max;
+        public ReturnData(boolean isBST, int min, int max) {
+            this.isBST = isBST;
+            this.max = max;
+            this.min = min;
+        }
+    }
+
+    public static ReturnData process(Node x) {
+        if (x == null) {
+            return null;
+        }
+        ReturnData leftData = process(x.left);
+        ReturnData rightData = process(x.right);
+
+        int min = x.val;
+        int max = x.val;
+        if (leftData != null) {
+            min = Math.min(min, leftData.min);
+            max = Math.max(max, leftData.max);
+        }
+        if (rightData != null) {
+            min = Math.min(min, rightData.min);
+            max = Math.max(max, rightData.max);
+        }
+
+        boolean isBST = true;
+        if (leftData != null && (!leftData.isBST || leftData.max >= x.val)) {
+            isBST = false;
+        }
+        if (rightData != null && (!rightData.isBST || rightData.min <= x.val)) {
+            isBST = false;
+        }
+
+        return new ReturnData(isBST, min, max);
+    }
 }
